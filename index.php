a932ead625a5dddb278c8bf0b7047e4c079df0c9
ajsciandra <andrewsciandra@comcast.net>
2018-01-04 00:35:29 -0500

    adding files back

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+  <?php include("includes/header.html");?>
+  <div class="container-fluid" style="padding-top : 10px">
+  <div class ="row">
+    <div class = "col-sm-4">Hugo Lloris </div>
+<!--  </div>-->
+
+<!--  <div class ="row">-->
+    <div class = "col-sm-4">Paulo Gazzaniga </div>
+<!--  </div>-->
+
+<!--  <div class ="row">-->
+    <div class = "col-sm-4">Michel Vorm</div>
+  </div>
+  <div class = "container" style = "padding-top: 10px">
+  <div class = "row">
+    <div class = "col-lg-6">
+      <div class = "thumbnail">
+        <a href = "Test.jpg" target = "_blank">
+        <img src = "Test.jpg" alt="celebration" style="width:100%">
+        <div class ="caption">
+          <p>Kane and Sonny Celebrating after Kane completes his hattrick against Southampton on December 26th 2017"</p>
+        </div>
+        </a>
+      </div>
+    </div>
+  </div>
+  
+</body>
+
+</html>
